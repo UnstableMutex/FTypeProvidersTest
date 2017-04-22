6f0b9e9f54a91c34c3ae359aed05097a6872b6d0
Dmitry <stormbringer-s@mail.ru>
2017-04-22 13:36:18 +0400

    not builded ver

--- FTypeProvidersTest/FTypeProvidersTest/Library1.fs
+++ FTypeProvidersTest/FTypeProvidersTest/Library1.fs
@@ -1,4 +1,10 @@
-﻿namespace FTypeProvidersTest
+﻿//namespace FTypeProvidersTest
+module Types
+open FSharp.Data
 
-type Class1() = 
-    member this.X = "F#"
+[<Literal>]
+let connectionString = 
+    @"Data Source=.;Initial Catalog=AdventureWorks2014;Integrated Security=True"
+
+type ShipMethod = SqlEnumProvider<"SELECT Name, ShipMethodID FROM Purchasing.ShipMethod ORDER BY ShipMethodID", connectionString>
+
---
+++ FTypeProvidersTest/FTypeProvidersTest/TP.fs
@@ -0,0 +1,28 @@
+﻿module TP
+open System
+open System.Reflection
+open Microsoft.FSharp.Core.CompilerServices
+open Microsoft.FSharp.Quotations
+
+[<TypeProvider>]
+type SampleTypeProvider(config: TypeProviderConfig) as this = 
+
+// Inheriting from this type provides implementations of ITypeProvider 
+// in terms of the provided types below.
+inherit TypeProviderForNamespaces()
+
+let namespaceName = "Samples.HelloWorldTypeProvider"
+let thisAssembly = Assembly.GetExecutingAssembly()
+
+// Make one provided type, called TypeN.
+let makeOneProvidedType (n:int) = 
+
+
+// Now generate 100 types
+let types = [ for i in 1 .. 100 -> makeOneProvidedType i ] 
+
+// And add them to the namespace
+do this.AddNamespace(namespaceName, types)
+
+[<assembly:TypeProviderAssembly>] 
+do()
